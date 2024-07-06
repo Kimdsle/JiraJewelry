180a5acd268a90e1d07e899d355a9ec555307dad
kim <82611452+Kimdsle@users.noreply.github.com>
2024-07-06 22:08:37 +0700

    Update UserRepository.java

--- src/main/java/com/jewelry/KiraJewelry/repository/UserRepository.java
+++ src/main/java/com/jewelry/KiraJewelry/repository/UserRepository.java
@@ -11,16 +11,16 @@ import com.jewelry.KiraJewelry.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query(value = "SELECT * FROM Users", nativeQuery = true)
+    @Query(value = "SELECT * FROM users", nativeQuery = true)
     List<User> findAllUsers();
 
-    @Query("SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
+    @Query("SELECT u FROM user u WHERE u.email = :email AND u.password = :password")
     User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
-    @Query("SELECT u FROM User u WHERE u.role_Id = :role_Id")
+    @Query("SELECT u FROM user u WHERE u.role_id = :role_id")
     List<User> findUsersByRoleId(@Param("role_Id") int role_Id);
 
-    @Query("SELECT u FROM User u WHERE u.user_Id = :user_Id")
+    @Query("SELECT u FROM user u WHERE u.user_id = :user_id")
     User findUsersByUserId(@Param("user_Id") int user_Id);
 
     User findByEmail (String email);
